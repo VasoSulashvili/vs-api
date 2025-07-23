45dd5a48abb66618518baff1b8d5a7d95ddb5598
Vaso Sulashvili <v.sulashvili@gmail.com>
2025-07-23 22:49:01 +0400

    added global api error exception handler

--- src/Providers/VSApiServiceProvider.php
+++ src/Providers/VSApiServiceProvider.php
@@ -5,8 +5,27 @@ declare(strict_types=1);
 namespace VSApi\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Exceptions\Handler;
 
 class VSApiServiceProvider extends ServiceProvider
 {
+
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        app()->make(Handler::class)->renderable(function (\Throwable $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $e->getMessage(),
+                    'errors' => []
+                ], $e->getStatusCode() ?: 500);
+            }
+        });
+    }
 
 }
